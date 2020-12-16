9fddd8369d8d4ff2615da522bddca9f36b2c988d
hosam <hosamelmalt@gmail.com>
2020-12-16 15:02:20 +0200

    Initial Commit

---
+++ Chattos/ChattosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattosApp.swift
+//  Chattos
+//
+//  Created by hosam on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct ChattosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
